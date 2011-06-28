dc358d8267fb7d66ceed1d394c8110545c12986d
Mathieu Bolla <bolla@algodeal.com>
2011-06-28 13:15:25 +0200

    Extract UI

--- src/main/java/com/mathieubolla/MassUpload.java
+++ src/main/java/com/mathieubolla/MassUpload.java
@@ -4,13 +4,10 @@ import static com.google.inject.name.Names.named;
 
 import java.io.File;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.inject.Named;
-import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 import com.amazonaws.ClientConfiguration;
@@ -18,75 +15,35 @@ import com.amazonaws.Protocol;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
+import com.mathieubolla.ui.SwingUi;
 
 public class MassUpload {
 	public static void main(String[] args) throws Exception {
 		Injector injector = Guice.createInjector(new MainModule());
 		ModularUpload upload = injector.getInstance(ModularUpload.class);
-		AmazonS3 s3 = injector.getInstance(AmazonS3.class);
+		SwingUi ui = injector.getInstance(SwingUi.class);
 		
 		System.setErr(new PrintStream(new File("/dev/null")));
 		final Queue<WorkUnit> toDos = new ConcurrentLinkedQueue<WorkUnit>();
 
-		UploadConfiguration configuration = configure(s3);
+		UploadConfiguration configuration = ui.configure();
 		if (confirm(message(configuration))) {
 			upload.process(toDos, configuration);
 		}
 	}
-	
-	private static UploadConfiguration configure(AmazonS3 s3) {
-		final String baseDir = chooseBaseDir() + "/";
-		if (baseDir.equals("null/")) {
-			throw new IllegalArgumentException("User choosed to abort");
-		}
-		
-		final String bucket = chooseBucket(s3, baseDir);
-		if (bucket == null) {
-			throw new IllegalArgumentException("User choosed to abort");
-		}
-		
-		final boolean shouldClearBucketFirst = chooseClearBucket(baseDir, bucket);
-		
-		return new UploadConfiguration(baseDir, bucket, shouldClearBucketFirst);
-	}
-	
+
 	private static String message(UploadConfiguration configuration) {
 		return "Will upload " + configuration.getBaseDirectory() + " on " + configuration.getBucketName() + ". Will "+(configuration.isClearBucketBeforeUpload() ? "" : "not ")+"clear it before uploading. Is this what you want?";
 	}
-	
+
 	private static boolean confirm(String message) {
 		return JOptionPane.showConfirmDialog(null, message) == JOptionPane.YES_OPTION;
 	}
-	
-	private static String chooseBucket(AmazonS3 s3, String forDirectory) {
-		List<String> options = new ArrayList<String>();
-		for (Bucket bucket : s3.listBuckets()) {
-			options.add(bucket.getName());
-		}
-		return options.get(JOptionPane.showOptionDialog(null, "Choose your Amazon S3 bucket to upload "+forDirectory, "Bucket Choice", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options.toArray(), options.toArray()[0]));
-	}
-	
-	private static boolean chooseClearBucket(String baseDir, String bucket) {
-		return JOptionPane.showOptionDialog(null, "Do you want to clear "+bucket+" prior to uploading "+baseDir+"?", "Clear Choice", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, JOptionPane.NO_OPTION) == JOptionPane.YES_OPTION;
-	}
 
-	private static String chooseBaseDir() {
-		String baseDir = null;
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		int showOpenDialogAnswer = fileChooser.showOpenDialog(null);
-		
-		if (showOpenDialogAnswer == JFileChooser.APPROVE_OPTION) {
-			baseDir = fileChooser.getSelectedFile().getAbsolutePath();
-		}
-		return baseDir;
-	}
-	
 	public static class MainModule extends AbstractModule {
 		@Override
 		protected void configure() {
---
+++ src/main/java/com/mathieubolla/ui/SwingUi.java
@@ -0,0 +1,62 @@
+package com.mathieubolla.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.mathieubolla.UploadConfiguration;
+
+public class SwingUi {
+	private final AmazonS3 amazonS3;
+
+	@Inject
+	public SwingUi(AmazonS3 amazonS3) {
+		this.amazonS3 = amazonS3;
+	}
+	
+	public UploadConfiguration configure() {
+		final String baseDir = chooseBaseDir() + "/";
+		if (baseDir.equals("null/")) {
+			throw new IllegalArgumentException("User choosed to abort");
+		}
+		
+		final String bucket = chooseBucket(baseDir);
+		if (bucket == null) {
+			throw new IllegalArgumentException("User choosed to abort");
+		}
+		
+		final boolean shouldClearBucketFirst = chooseClearBucket(baseDir, bucket);
+		
+		return new UploadConfiguration(baseDir, bucket, shouldClearBucketFirst);
+	}
+	
+	
+	private String chooseBucket(String forDirectory) {
+		List<String> options = new ArrayList<String>();
+		for (Bucket bucket : amazonS3.listBuckets()) {
+			options.add(bucket.getName());
+		}
+		return options.get(JOptionPane.showOptionDialog(null, "Choose your Amazon S3 bucket to upload "+forDirectory, "Bucket Choice", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options.toArray(), options.toArray()[0]));
+	}
+	
+	private boolean chooseClearBucket(String baseDir, String bucket) {
+		return JOptionPane.showOptionDialog(null, "Do you want to clear "+bucket+" prior to uploading "+baseDir+"?", "Clear Choice", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, JOptionPane.NO_OPTION) == JOptionPane.YES_OPTION;
+	}
+
+	private String chooseBaseDir() {
+		String baseDir = null;
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int showOpenDialogAnswer = fileChooser.showOpenDialog(null);
+		
+		if (showOpenDialogAnswer == JFileChooser.APPROVE_OPTION) {
+			baseDir = fileChooser.getSelectedFile().getAbsolutePath();
+		}
+		return baseDir;
+	}
+}
