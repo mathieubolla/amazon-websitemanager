ec546b5d2b7faa085fc79a7aeffe015bc393a0aa
Mathieu Bolla <bolla@algodeal.com>
2011-06-28 16:02:43 +0200

    Test delete task

--- src/main/java/com/mathieubolla/processing/DeleteUnit.java
+++ src/main/java/com/mathieubolla/processing/DeleteUnit.java
@@ -1,7 +1,6 @@
 package com.mathieubolla.processing;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class DeleteUnit extends WorkUnit {
@@ -13,7 +12,7 @@ public class DeleteUnit extends WorkUnit {
 
 	@Override
 	public void doJob(AmazonS3 s3) {
-		s3.deleteObject(new DeleteObjectRequest(summary.getBucketName(), summary.getKey()));
+		s3.deleteObject(summary.getBucketName(), summary.getKey());
 		System.out.println("Successfully deleted "+summary);
 	}
 	
---
+++ src/test/java/com/mathieubolla/processing/DeleteUnitTest.java
@@ -0,0 +1,26 @@
+package com.mathieubolla.processing;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class DeleteUnitTest {
+	@Test
+	public void shouldDeleteSummaryOnS3() {
+		S3ObjectSummary mockS3ObjectSummary = mock(S3ObjectSummary.class);
+		AmazonS3 mockAmazonS3 = mock(AmazonS3.class);
+		when(mockS3ObjectSummary.getBucketName()).thenReturn("bucket");
+		when(mockS3ObjectSummary.getKey()).thenReturn("key");
+		
+		DeleteUnit deleteUnit = new DeleteUnit(mockS3ObjectSummary);
+		
+		deleteUnit.doJob(mockAmazonS3);
+		
+		verify(mockAmazonS3).deleteObject("bucket", "key");
+	}
+}
