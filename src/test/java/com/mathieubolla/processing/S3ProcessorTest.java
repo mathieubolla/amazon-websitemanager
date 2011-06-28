34f52fe047519954d09bdd2beb1f245f095732fe
Mathieu Bolla <bolla@algodeal.com>
2011-06-28 16:06:05 +0200

    Test multithreaded processor

--- src/test/java/com/mathieubolla/processing/S3ProcessorTest.java
+++ src/test/java/com/mathieubolla/processing/S3ProcessorTest.java
@@ -9,7 +9,10 @@ import static org.mockito.Mockito.when;
 import java.io.File;
 import java.util.Date;
 import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.fest.assertions.Fail;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,7 +22,6 @@ import com.mathieubolla.UploadConfiguration;
 import com.mathieubolla.io.DirectoryScanner;
 import com.mathieubolla.io.S3Scanner;
 
-
 public class S3ProcessorTest {
 	S3Processor s3Processor;
 	S3Scanner mockS3Scanner;
@@ -27,7 +29,7 @@ public class S3ProcessorTest {
 	AmazonS3 mockAmazonS3;
 	Queue<WorkUnit> mockQueue;
 	UploadConfiguration mockUploadConfiguration;
-	
+
 	@Before
 	@SuppressWarnings("unchecked")
 	public void setup() {
@@ -38,37 +40,37 @@ public class S3ProcessorTest {
 		mockUploadConfiguration = mock(UploadConfiguration.class);
 		s3Processor = new S3Processor(mockAmazonS3, mockS3Scanner, mockDirectoryScanner, mockQueue);
 	}
-	
+
 	@Test
 	public void shouldAcceptDeleteCommands() {
 		UploadConfiguration mockUploadConfiguration = mock(UploadConfiguration.class);
 		S3ObjectSummary mockS3ObjectSummary = mock(S3ObjectSummary.class);
-		
+
 		when(mockUploadConfiguration.getBucketName()).thenReturn("bucket");
 		when(mockS3Scanner.listObjects("bucket")).thenReturn(asList(mockS3ObjectSummary));
-		
+
 		when(mockUploadConfiguration.isClearBucketBeforeUpload()).thenReturn(true);
 
 		s3Processor.clearBucket(mockUploadConfiguration);
-		
+
 		verify(mockQueue).add(new DeleteUnit(mockS3ObjectSummary));
 	}
-	
+
 	@Test
 	public void shouldRefuseDeleteCommands() {
-		
+
 		S3ObjectSummary mockS3ObjectSummary = mock(S3ObjectSummary.class);
-		
+
 		when(mockUploadConfiguration.getBucketName()).thenReturn("bucket");
 		when(mockS3Scanner.listObjects("bucket")).thenReturn(asList(mockS3ObjectSummary));
 
 		when(mockUploadConfiguration.isClearBucketBeforeUpload()).thenReturn(false);
-		
+
 		s3Processor.clearBucket(mockUploadConfiguration);
-		
+
 		verifyNoMoreInteractions(mockQueue);
 	}
-	
+
 	@Test
 	public void shouldAcceptUploadCommands() {
 		Date mockDate = mock(Date.class);
@@ -78,9 +80,45 @@ public class S3ProcessorTest {
 		when(mockDirectoryScanner.scanRegularFiles(mockDirectory)).thenReturn(asList(mockFile));
 		when(mockUploadConfiguration.getBaseDirectory()).thenReturn(mockDirectory);
 		when(mockUploadConfiguration.uploadUnitFor(mockFile, mockDate)).thenReturn(mockUploadUnit);
-		
+
 		s3Processor.uploadBucket(mockUploadConfiguration, mockDate);
-		
+
 		verify(mockQueue).add(mockUploadUnit);
+	}
+
+	@Test(timeout = 1000)
+	public void shouldProcessQueueWith10Threads() {
+		s3Processor = new S3Processor(mockAmazonS3, mockS3Scanner, mockDirectoryScanner, buildNDependentTasks(10));
+
+		s3Processor.processQueue();
+	}
+
+	private Queue<WorkUnit> buildNDependentTasks(int n) {
+		final AtomicInteger sharedLock = new AtomicInteger(0);
+		Queue<WorkUnit> queue = new LinkedBlockingDeque<WorkUnit>();
+		for (int i = 0; i < n; i++) {
+			queue.add(lockNThreadsTask(sharedLock, n));
+		}
+		return queue;
+	}
+
+	private WorkUnit lockNThreadsTask(final AtomicInteger lock, final int n) {
+		return new WorkUnit() {
+			@Override
+			public void doJob(AmazonS3 s3) {
+				synchronized (lock) {
+					System.out.println(lock);
+					if(lock.incrementAndGet() < n) {
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							Fail.fail("Should not get interrupted");
+						}
+					} else {
+						lock.notifyAll();
+					}
+				}
+			}
+		};
 	}
 }
